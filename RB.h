8112f3e36e49f2f0a817ec3ce253a7cbcd4045f9
hossam762 <karimaboshamia@gmail.com>
2022-01-05 14:37:25 +0200

    RB tree

---
+++ RB.h
@@ -0,0 +1,14 @@
+//
+// Created by Rocker on 12/25/2021.
+//
+
+#ifndef P10_RBT_2_RB_H
+#define P10_RBT_2_RB_H
+
+
+class RB {
+
+};
+
+
+#endif //P10_RBT_2_RB_H
---
+++ cmake-build-debug/CMakeFiles/P10_RBT_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P10_RBT_2.dir/RB.cpp.obj"
+  "P10_RBT_2.exe"
+  "P10_RBT_2.exe.manifest"
+  "P10_RBT_2.pdb"
+  "libP10_RBT_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P10_RBT_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
